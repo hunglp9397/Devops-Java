99d8cef2d39f0d9e092e77af7cd3e0d3673d5270
hunglp9397 <lephihung0997@gmail.com>
2023-08-03 02:30:21 +0700

    init project

---
+++ CI_CD_Java_Spring/basic-springboot-cicd/src/main/java/com/hunglp/basicspringbootcicd/BasicSpringbootCicdApplication.java
@@ -0,0 +1,13 @@
+package com.hunglp.basicspringbootcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSpringbootCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicSpringbootCicdApplication.class, args);
+	}
+
+}
---
+++ CI_CD_Java_Spring/basic-springboot-cicd/src/test/java/com/hunglp/basicspringbootcicd/BasicSpringbootCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hunglp.basicspringbootcicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicSpringbootCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
